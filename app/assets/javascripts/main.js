163200722055f6e58929267cb06285c40c3aafbc
Tom Dyer <tdyer@ourstage.com>
2014-03-11 11:02:44 -0400

    Added the angular directory structure

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,9 @@
-// TODO: Add angular application module and routes.
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+//= require_self
+//= require_tree ./services/main
+//= require_tree ./filters/main
+//= require_tree ./controllers/main
+//= require_tree ./directives/main
+
+var StoreFront = angular.module('StoreFront',['ngRoute']);
