1678557ef9fcbf928a17ecb52699755aef8ea1c7
Tom Dyer <tdyer@ourstage.com>
2014-03-11 10:53:30 -0400

    Turn off generation of helpers, assets and specs for controller and views

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,14 @@ module AngularProducts
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #  turn off generation of helpers, assets, controller and view specs
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.controller_specs false
+      generate.view_specs false
+    end
+
   end
 end
