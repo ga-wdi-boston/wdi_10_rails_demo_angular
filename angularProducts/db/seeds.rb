5773faed2ea65d5d6e63170cbe98dd08b02664f0
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-11 15:47:50 -0400

    created seeds in the seed file

--- angularProducts/db/seeds.rb
+++ angularProducts/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+product = Product.create( name: "Boot", description: "Shiny and black", price: 108.76)
+puts "created product: #{product.inspect}"
+product = Product.create( name: "Shoe Horn", description: "About 3 feet long, with a built in radio", price: 6.24)
+puts "created product: #{product.inspect}"
