165c05dab0c4cb71ba6013862e293eeb5ffe89e8
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-11 14:09:18 -0400

    added angularProducts rails app and cleaned up database yml

---
+++ angularProducts/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularProducts::Application.config.secret_key_base = '53ce5515f3c123cb625a2f26d78089082d15d2d2180911d391cdfdcba86e46e664fd5b18a04963558c4ac19403177824b9b4679601a966e0b0f34d30ad053fc9'
