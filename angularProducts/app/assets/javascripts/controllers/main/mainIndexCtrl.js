32910163de61d2f8e67c240d7192b88ece33ce40
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-11 15:43:35 -0400

    updated the mainIndexCtrl.js

--- angularProducts/app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ angularProducts/app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,12 +1,12 @@
 var IndexCtrl = function($scope, $location, $http){
-    $scope.data = {};
-    $scope.data.products = [
-        {"name": "Shoe", "description": "Old Red smelly", "price": 0.22 },
-        {"name": "Frisbee", "description": "Silver with a gerbil taped to it" ,  "price": 7.63 }
-    ];
+    // $scope.data = {};
+    // $scope.data.products = [
+    //     {"name": "Shoe", "description": "Old Red smelly", "price": 0.22 },
+    //     {"name": "Frisbee", "description": "Silver with a gerbil taped to it" ,  "price": 7.63 }
+    // ];
 
-    // $scope.data = productData;
-    // productData.loadProducts();
+    $scope.data = productData;
+    productData.loadProducts();
 
     $scope.viewPost = function(productId){
         $location.url('/product/' + productId);
